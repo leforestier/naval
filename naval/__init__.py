d240214c7b05f88e5bce9c27d045e543ac4c6f94
Benjamin Le Forestier <leforestier@users.noreply.github.com>
2016-01-08 11:55:02 +0100

    version 0.5.1

--- naval/__init__.py
+++ naval/__init__.py
@@ -93,7 +93,7 @@ Supply a ``lang`` keyword argument to the ``validate`` method to obtain translat
 """
 
 __author__ = "Benjamin Le Forestier (benjamin@leforestier.org)"
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 from naval.core import *
 from naval.util import Email, Url
